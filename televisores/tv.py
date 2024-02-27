6a0b34c299a0c0317bcaaa5089e8d2b12ec0ef5a
Jedett <159485815+Jedett@users.noreply.github.com>
2024-02-27 00:24:07 -0500

    Taller 3 - PYTHON V2

---
+++ televisores/tv.py
@@ -0,0 +1,68 @@
+class TV:
+    _numTV=0
+    def __init__(self, marca, estado):
+        self._marca=marca
+        self._estado=estado
+        self._canal=1
+        self._volumen=1
+        self._precio=500
+        self._control=0
+        TV._numTV+=1
+        
+    @classmethod
+    def setNumTV(cls, numTV):
+        cls._numTV=numTV
+    def getNumTV():
+        return TV._numTV
+         
+    def getMarca(self):
+        return self._marca
+    def setMarca(self, marca):
+        self._marca=marca
+
+    def getCanal(self):
+        return self._canal
+    def setCanal(self, canal):
+        if (canal<=120 and canal>=1 and self._estado==True):
+            self._canal=canal
+ 
+    def getPrecio(self):
+        return self._precio
+    def setPrecio(self, precio):
+        self._precio=precio
+ 
+    def getVolumen(self):
+        return self._volumen
+    def setVolumen(self, volumen):
+        if(volumen>=0 and volumen<=7 and self._estado==True):
+            self._volumen=volumen
+
+    def getControl(self):
+        return self._control
+    def setControl(self, control):
+        self._control=control
+
+    def turnOn(self):
+       self._estado=True
+    def turnOff(self):
+        self._estado=False
+
+    def getEstado(self):
+        return self._estado
+    def setEstado(self, estado):
+        self._estado=estado
+
+    def canalUp(self):
+        if(self._canal<120 and self._estado==True):
+            self._canal+=1
+    def canalDown(self):
+        if (self._canal>1 and self._estado==True):
+            self._canal-=1
+
+    def volumenUp(self):
+        if(self._volumen<7 and self._estado==True):
+            self._volumen+=1
+    def volumenDown(self):
+        if (self._volumen>0 and self._estado==True):
+            self._volumen-=1
+      
